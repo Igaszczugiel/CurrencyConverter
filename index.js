0dca58dd22545ae1287f615badf657733158799a
Igaszczugiel <78268100+Igaszczugiel@users.noreply.github.com>
2021-02-15 19:31:25 +0000

    under construction

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-let body = document.querySelector("body");
-// 01 input w ktory wpisywana jest ilosc do przeliczenia
-let number = document.getElementById("number");
-// 03 button do convertowania
-let button = document.getElementById("button");
-// 04 here tu pojawi sie resultat
-let result = document.getElementById("result");
-
 // <!-- JS 00 / show list of available currencies PLN -->
 // <!-- JS 01 / type how much to convert -->
 // <!-- JS 02 / show list of available currencies -->
 // <!-- JS 03 / button ktory on click triggers 01 -->
-
 
 // url dla id pln = http://api.nbp.pl/api/exchangerates/rates/a/pln/
 // url dla id euro = http://api.nbp.pl/api/exchangerates/rates/a/eur/
@@ -18,23 +9,23 @@ let result = document.getElementById("result");
 // url dla id franc = http://api.nbp.pl/api/exchangerates/rates/a/chf/?format=json
 
 
-function convertCurrency() {
-    let url = 'http://api.nbp.pl/api/exchangerates/rates/a/';
-    let finalAmount = document.getElementById('currencyListFrom').options[document.getElementById('currencyListFrom').selectedIndex].value;
-    let currency = finalAmount;
-    let urlResult = (url + currency+'/?format=json');
+let result = document.getElementById("result");
+let form = document.querySelector('#convertIt');
 
-    fetch(urlResult).then(resp => resp.json()).then((response) => {
-        // let franc = przypisz wartosc 
+form.addEventListener("submit", convertCurrency);
+
+function convertCurrency(event) {
+    event.preventDefault();
+    // to stopuje forme przed naturalnym zachowaniem 
+    // (przeladowanie strony - default behaviour)
+    let currencyCode = document.querySelector('#currencyListFrom').value;
+    let url = `http://api.nbp.pl/api/exchangerates/rates/a/${currencyCode}/?format=json`;
+
+    fetch(url).then(resp => resp.json()).then((response) => {
         console.log(response.rates[0].mid);
-
-        // final = {
-        //     mid: response.rates[0].mid,
-        // }   
-        // let sum = final.mid * number.value
-       
-        // result.innerHTML = sum
+        let currencyRate = response.rates[0].mid;
+        let amount = document.getElementById("amount").value;
+        let total = currencyRate * amount;
+        result.innerHTML = total;
     })
 }; 
-
-convertCurrency();
