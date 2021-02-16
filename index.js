64408bbf7084c51a3e7c09481978dd7e7be7876c
Igaszczugiel <78268100+Igaszczugiel@users.noreply.github.com>
2021-02-16 17:37:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function convertCurrency(event) {
     event.preventDefault();
     // this stops form from default behaviour (page reload)
     let currencyCode = document.querySelector('#currencyListFrom').value;
-    let url = `http://api.nbp.pl/api/exchangerates/rates/a/${currencyCode}/?format=json`;
+    let url = `https://api.nbp.pl/api/exchangerates/rates/a/${currencyCode}/?format=json`;
 
     fetch(url).then(resp => resp.json()).then((response) => {
         console.log(response.rates[0].mid);
